0f9179ec4615470c4bb9ee64f0751581e8720ddc
Pap3redits <43688486+Pap3redits@users.noreply.github.com>
2021-04-09 13:14:00 -0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 #list of apps to install
-Apt_List="trash-cli neofetch vim snapd discord steam net-tools piper htop gnome-tweaks vlc sysstat"
+Apt_List="trash-cli neofetch vim snapd discord steam net-tools piper htop gnome-tweaks vlc mlocate flatpak"
 Snap_List="spotify"
 
 
@@ -12,19 +12,12 @@ sudo apt upgrade -y
 #create bin folder
 sudo mkdir ~/bin
 
-#create update script
-sudo touch ~/bin/update
-sudo echo '#!/bin/bash' >> ~/bin/update
-sudo echo 'sudo apt update' >> ~/bin/update
-sudo echo 'sudo apt upgrade -y' >> ~/bin/update
+#copy update script to ~/bin folder
+cp update.sh ~/bin/.
 sudo chmod 755 ~/bin/update
 
-#create sysclean script
-sudo touch ~/bin/sysclean
-sudo echo '#!/bin/bash' >> ~/bin/sysclean
-sudo echo 'sudo apt autoremove' >> ~/bin/sysclean
-sudo echo 'trash-empty' >> ~/bin/sysclean
-sudo echo 'trash has been cleaned' >> ~/bin/sysclean
+#copy sysclean script to ~/bin folder
+cp sysclean.sh ~/bin/.
 sudo chmod 755 ~/bin/sysclean
 
 #install listed programs
@@ -32,7 +25,7 @@ sudo apt install -y $Apt_List
 sudo snap install  $Snap_List
 
 #install MangoHud
-sudo git clone --recurse-submodules https://github.com/flightlessmango/MangoHud.git
-sudo MangoHud/build.sh build
-sudo MangoHud/build.sh package
-sudo MangoHud/build.sh install
+sudo git clone --recurse-submodules https://github.com/flightlessmango/MangoHud.git ~/MangoHud
+sudo ~/MangoHud/build.sh build
+sudo ~/MangoHud/build.sh package
+sudo ~/MangoHud/build.sh install
