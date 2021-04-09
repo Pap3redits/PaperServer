c927dfe64d36204b660bd804ef66f2495cee9284
Pap3redits <43688486+Pap3redits@users.noreply.github.com>
2021-04-09 12:53:47 -0400

    Create update

---
+++ update.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+
+**********************
+Updating apt packages
+**********************
+
+sudo apt update
+sudo apt upgrade -y
+
+**********************
+Updating flatpaks 
+**********************
+update flatpak -y
+
+**********************
+Updating snaps
+**********************
+
+sudo snap refresh
+
+**********************
+Update Completed!
+**********************
+
